3b38b04065f1c75fb17403bff61dba6071311e4b
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-14 14:00:55 +0700

    first commit

---
+++ Services/Service.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chetch.Services
+{
+    abstract public class Service<T> : BackgroundService where T : BackgroundService
+    {
+        public const String APPSETTINGS_FILE = "appsettings.json";
+
+        static public IConfiguration Config {get; internal set; } = null;
+
+        static public void Run(String[] args)
+        {
+            var configBuilder = new ConfigurationBuilder().AddJsonFile(APPSETTINGS_FILE, false);
+            Config = configBuilder.Build();
+
+            var builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddSystemd();
+            builder.Services.AddHostedService<T>();
+
+            var host = builder.Build();
+            host.Run();
+        }
+
+        public ILogger<T> Logger { get; protected set; }
+
+        public Service(ILogger<T> logger){
+            Logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation(10, "Starting service at: {time}", DateTimeOffset.Now);
+            return base.StartAsync(cancellationToken);
+        }
+
+        abstract protected Task Execute(CancellationToken stoppingToken);
+        
+        protected void OnError(Exception ex, int eventID = 0)
+        {
+            Logger.LogError(eventID, ex, "Exception: {0}", ex.Message);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                Logger.LogInformation(100, "Service started executing at: {time}", DateTimeOffset.Now);
+
+                await Execute(stoppingToken);
+                
+                Logger.LogInformation(100, "Service finished executing at: {time}", DateTimeOffset.Now);
+            
+            }
+            catch (OperationCanceledException)
+            {
+                Logger.LogWarning(100, "Service cancelled at: {time}", DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex);
+            }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation(1000, "Stopping service at: {time}", DateTimeOffset.Now);
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
