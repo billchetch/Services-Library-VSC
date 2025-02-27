d6b5c02716f55cdd0e35ae4f8697eeeebe6ddcc2
billchetch <45006134+billchetch@users.noreply.github.com>
2025-02-27 21:38:14 +0700

    asdf

---
+++ Services.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestConstruction()
+    {
+        Console.WriteLine("This does not output");
+    }
+}
