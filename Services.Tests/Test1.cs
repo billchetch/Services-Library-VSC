8f3f49961ba5655582fd538c3e0c376d543c1847
billchetch <45006134+billchetch@users.noreply.github.com>
2025-02-27 22:05:52 +0700

    sdfs

--- Services.Tests/Test1.cs
+++ Services.Tests/Test1.cs
@@ -1,11 +1,40 @@
-﻿namespace Services.Tests;
+﻿using System.Diagnostics;
+using Chetch.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+
+namespace Services.Tests;
 
 [TestClass]
 public sealed class Test1
 {
+    public class TestService : Service<TestService>
+    {
+        public TestService(ILogger<TestService> logger) : base(logger)
+        {
+        }
+
+        protected override Task Execute(CancellationToken stoppingToken)
+        {
+            return Task.Delay(100);
+        }
+    }
+
     [TestMethod]
     public void TestConstruction()
     {
-        Console.WriteLine("This does not output");
+        var factory = new LoggerFactory();
+        var logger = new Logger<TestService>(factory);
+
+        for(int i = 0; i < 130; i++)
+        {
+            try
+            {
+                var ts = new TestService(logger);
+            } catch (Exception e)
+            {
+                Debug.Print(e.Message);
+            }
+        }
     }
 }
